35de37ce9b044569ec756d2f696d1d02dc7dc39c
seoyoung33 <bsy4357@gmail.com>
2025-07-15 18:17:21 +0900

    ability 정리중

--- scripts/index.js
+++ scripts/index.js
@@ -2,3 +2,12 @@ const wrap = new Swiper('#wrap',{
     direction:'vertical',
     mousewheel:true,
 })
+
+/* ability 슬라이드 */
+const ability = new Swiper('#ability_swiper',{
+    scrollbar:{el:'#ability_swiper~.swiper-scrollbar'},
+    navigation:{
+        nextEl:'#ability_swiper~.swiper-button-next',
+        prevEl:'#ability_swiper~.swiper-button-prev',
+    }
+})
