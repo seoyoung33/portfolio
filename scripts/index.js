48a6a38ced288f609cbabf483f1e09c60a201c92
seoyoung33 <bsy4357@gmail.com>
2025-07-15 21:16:07 +0900

    내일은 마무리하기

--- scripts/index.js
+++ scripts/index.js
@@ -11,3 +11,20 @@ const ability = new Swiper('#ability_swiper',{
         prevEl:'#ability_swiper~.swiper-button-prev',
     }
 })
+
+/* 프로젝트 슬라이드 */
+const project1 = new Swiper('#web_design_swiper',{
+    scrollbar:{el:'#web_design_swiper~.swiper-scrollbar'},
+    navigation:{
+        nextEl:'#web_design_swiper~.swiper-button-next',
+        prevEl:'#web_design_swiper~.swiper-button-prev',
+    }
+})
+
+const etc_design = new Swiper('#etc_design_swiper',{
+    slidesPerView:3,
+    spaceBetween:10,
+    autoplay:{delay:0,},
+    speed:8000,
+    loop:true,
+});
