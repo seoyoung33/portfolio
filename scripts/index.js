ccceb530ca251ae9b596f01117081d2fe82e1791
seoyoung33 <bsy4357@gmail.com>
2025-07-16 18:25:57 +0900

    마무리하자

--- scripts/index.js
+++ scripts/index.js
@@ -21,10 +21,42 @@ const project1 = new Swiper('#web_design_swiper',{
     }
 })
 
-const etc_design = new Swiper('#etc_design_swiper',{
-    slidesPerView:3,
+/* 배너디자인 스와이퍼 */
+const banner_design = new Swiper('#banner_swiper',{
+    slidesPerView:2,
     spaceBetween:10,
     autoplay:{delay:0,},
     speed:8000,
     loop:true,
 });
+
+/* ect디자인 스와이퍼 */
+const etc_design = new Swiper('#etc_design_swiper',{
+    slidesPerView:3,
+    spaceBetween:10,
+    autoplay:{delay:0,},
+    speed:10000,
+    loop:true,
+});
+
+/* detail 디자인 스와이퍼 */
+const detail_design = new Swiper('#detail_swiper',{
+    slidesPerView:1,
+    spaceBetween:10,
+    autoplay:{delay:0,},
+    speed:10000,
+    loop:true,
+    direction:'vertical',
+});
+
+const nav = document.querySelectorAll('nav a');
+//수직 스와이프 이동 함수
+//수직스와이프변수명.slideTop(이동인덱스값, 지속시간)
+
+nav.forEach((obj, idx)=>{
+    console.log(obj, idx);
+    obj.addEventListener('click',(e)=>{
+        e.preventDefault();//a의 href기본기능 막기
+        wrap.slideTo(idx, 1000);
+    })
+})
